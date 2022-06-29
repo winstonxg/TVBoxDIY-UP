7b91c63261b88c0d1a63e9af306021feca57fe6a
lm317379829 <lm317379829@163.com>
2022-06-29 16:35:44 +0800

    Update diy-dev.sh

--- diy-dev.sh
+++ diy-dev.sh
@@ -22,17 +22,18 @@ lg1=$(sed -n  '/R.id.tvLang/=' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/githu
 lg0=$(sed -n  '/R.id.tvArea/=' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java)
 let lg2=$lg0-1
 sed -i "${lg1},${lg2}d" $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
-lh1=$(sed -n  '/R.id.tvLang/=' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java)
-lh0=$(sed -n  '/R.id.tvArea/=' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java)
+lh1=$(sed -n  '/R.id.tvLang/=' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java | head -n 1)
+lh0=$(sed -n  '/R.id.tvArea/=' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java | head -n 1)
 let lh2=$lh0-1
 sed -i "${lh1},${lh2}d" $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
+sed -i '/tvLang/d' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
 la1=$(sed -n  '/@+id\/tvLang/=' $CURRENT_DIR/TVBoxOSC/app/src/main/res/layout/activity_detail.xml)
 la0=$(sed -n  '/@+id\/tvType/=' $CURRENT_DIR/TVBoxOSC/app/src/main/res/layout/activity_detail.xml)
 let la2=$la0-1
 sed -i "${la1},${la2}d" $CURRENT_DIR/TVBoxOSC/app/src/main/res/layout/activity_detail.xml
 li0=$(sed -n  '/@+id\/tvLang/=' $CURRENT_DIR/TVBoxOSC/app/src/main/res/layout/item_grid.xml)
 let li1=$li0-1
-li3=$(sed -n  '/\/LinearLayout/=' $CURRENT_DIR/TVBoxOSC/app/src/main/res/layout/item_grid.xml| head -n 1)
+li3=$(sed -n  '/\/LinearLayout/=' $CURRENT_DIR/TVBoxOSC/app/src/main/res/layout/item_grid.xml | head -n 1)
 let li2=$li3-1
 sed -i "${li1},${li2}d" $CURRENT_DIR/TVBoxOSC/app/src/main/res/layout/item_grid.xml
 
