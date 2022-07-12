6780d9fc0e54df81c5f877608cbf63ac9959624c
lm317379829 <lm317379829@163.com>
2022-07-12 11:54:23 +0800

    Update diy-dev.sh

--- diy-dev.sh
+++ diy-dev.sh
@@ -2,7 +2,8 @@
 
 #获取目录
 CURRENT_DIR=$(cd $(dirname $0); pwd)
-
+#名称修改
+sed -i 's/TVBox/极影视/g' $CURRENT_DIR/TVBoxOSC/app/src/main/res/values/strings.xml
 #背景修改
 mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/TVBoxOSC/app/src/main/res/drawable/app_bg.png
 #缩略图清晰度修改
