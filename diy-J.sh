ddc7d30556198e1d89b92d39617bfcceb9745030
lm317379829 <lm317379829@163.com>
2022-07-28 15:45:32 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -7,4 +7,10 @@ sed -i 's/TVBox/极影视/g' $CURRENT_DIR/TVBoxOSC/app/src/main/res/values/strin
 #背景修改
 mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/TVBoxOSC/app/src/main/res/drawable/app_bg.png
 
+
+#进入目录
+num=$(find $CURRENT_DIR -name gradlew  | awk -F"/" '{print NF-1}')
+DIR=$(find $CURRENT_DIR -name gradlew  | cut -d \/ -f$num)
+cd $DIR
+
 echo 'DIY end'
