313b8a49e163422ccbe46b1639e5654c4565e2ad
lm317379829 <lm317379829@163.com>
2022-07-28 09:48:07 +0800

    Update diy-main.sh

--- diy-main.sh
+++ diy-main.sh
@@ -6,5 +6,16 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 sed -i 's/TVBox/极影视/g' $CURRENT_DIR/TVBoxOSC/app/src/main/res/values/strings.xml
 #背景修改
 mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/TVBoxOSC/app/src/main/res/drawable/app_bg.png
+#缩略图清晰度修改
+sed -i 's/mContext, 400/mContext, 500/g' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+sed -i 's/mContext, 300/mContext, 400/g' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+sed -i 's/mContext, 400/mContext, 500/g' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
+sed -i 's/mContext, 300/mContext, 400/g' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
+sed -i 's/mContext, 400/mContext, 500/g' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
+sed -i 's/mContext, 300/mContext, 400/g' $CURRENT_DIR/TVBoxOSC/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
+#解决模拟器闪退
+echo "" >>$CURRENT_DIR/TVBoxOSC/app/proguard-rules.pro 
+echo "#新增设定" >>$CURRENT_DIR/TVBoxOSC/app/proguard-rules.pro 
+echo "-keep class com.xunlei.downloadlib.** {*;}" >>$CURRENT_DIR/TVBoxOSC/app/proguard-rules.pro 
 
 echo 'DIY End'
