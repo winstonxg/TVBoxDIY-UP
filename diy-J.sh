5e4b4e1b23dead75b53b6cfb3ee8f056d557a312
lm317379829 <lm317379829@163.com>
2022-07-29 16:52:59 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -9,5 +9,7 @@ cd $DIR
 sed -i 's/TVBox/极影视/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
 #背景修改
 mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_bg.png
+#修正播放控制
+sed -i 's/重播本集/重播/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
 
 echo 'DIY end'
