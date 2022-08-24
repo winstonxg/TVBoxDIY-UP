6cd6cc702b21a78ec4cb3ada6d2917c499268118
lm317379829 <lm317379829@163.com>
2022-08-24 09:00:14 +0800

    Update diy-T.sh

--- diy-T.sh
+++ diy-T.sh
@@ -21,6 +21,7 @@ sed -i 's/mContext, 400/mContext, 500/g' $CURRENT_DIR/$DIR/app/src/main/java/com
 sed -i 's/mContext, 300/mContext, 400/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
 sed -i 's/mContext, 400/mContext, 500/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
 sed -i 's/mContext, 300/mContext, 400/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/adapter/HistoryAdapter.java
+#添加网速显示
 #mv $CURRENT_DIR/DIY/BaseController.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/player/controller/BaseController.java
 #mv $CURRENT_DIR/DIY/player_vod_control_view.xml $CURRENT_DIR/$DIR//app/src/main/res/layout/player_vod_control_view.xml
 #mv $CURRENT_DIR/DIY/player_live_control_view.xml $CURRENT_DIR/$DIR//app/src/main/res/layout/player_live_control_view.xml
