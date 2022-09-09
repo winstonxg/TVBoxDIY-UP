71ef7621abfe91b0c178ec78830d04ae658e48bd
lm317379829 <lm317379829@163.com>
2022-09-09 14:04:18 +0800

    Update diy-O.sh

--- diy-O.sh
+++ diy-O.sh
@@ -5,6 +5,8 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 num=$(find $CURRENT_DIR -name gradlew  | awk -F"/" '{print NF-1}')
 DIR=$(find $CURRENT_DIR -name gradlew  | cut -d \/ -f$num)
 cd $DIR
+#xwalk修复
+sed -i 's/https:\/\/download.01.org\/crosswalk\/releases\/crosswalk\/android\/maven2/http:\/\/9xi4o.tk\/maven2/g' $CURRENT_DIR/$DIR/build.gradle
 #名称修改
 sed -i 's/TVBox/极影视/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
 #背景修改
