210a07c9479f8b64f9d542762e67dbe7660c0600
lm317379829 <lm317379829@163.com>
2022-09-14 11:18:38 +0800

    Update diy-O.sh

--- diy-O.sh
+++ diy-O.sh
@@ -2,8 +2,8 @@
 
 #获取目录
 CURRENT_DIR=$(cd $(dirname $0); pwd)
-num=$(find $CURRENT_DIR -name gradlew  | awk -F"/" '{print NF-1}')
-DIR=$(find $CURRENT_DIR -name gradlew  | cut -d \/ -f$num)
+num=$(find $CURRENT_DIR -name gradlew  | awk -F"/" '{print NF-1}'| head -1)
+DIR=$(find $CURRENT_DIR -name gradlew  | cut -d \/ -f$num | head -1)
 cd $DIR
 #xwalk修复
 sed -i 's/download.01.org\/crosswalk\/releases\/crosswalk\/android\/maven2/raw.githubusercontent.com\/lm317379829\/TVBoxDIY\/main/g' $CURRENT_DIR/$DIR/build.gradle
